dc61b0351cf5cbcafd941701b3283adb58d7df92
SpiritCK <kevin_jonathan_k@yahoo.co.id>
2017-07-25 23:26:30 +0700

    New, refreshing view :D

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,6 +18,12 @@
 
         <!-- Styles -->
         <style>
+            html {
+                height:100%;
+            }
+            body {
+                height:100%;
+            }
             th, td {
                 padding: 2px;
                 text-align: left;
@@ -27,6 +33,50 @@
                 width: 300px;
                 text-align: center;
                 color:#000;
+            }
+
+            h3.accordion{
+                margin: 0;
+            }
+            accordion a{
+                border-radius: 5px;
+                color: #333;
+                text-decoration: none;
+                display: block;
+                margin: 3px 10px;
+                padding: 0px 15px;
+            }
+            .panel-heading{
+                padding: 0px;
+            }
+            .panel-body{
+                background-image: url("image/background/content.png");
+                background-size: 100% 100%;
+                padding: 15px 30px;
+            }
+            div.container {
+                width: 100%;
+                border: 1px solid gray;
+            }
+
+            header, footer {
+                padding: 1em;
+                color: white;
+                background-color: black;
+                clear: left;
+                text-align: center;
+                min-height: 250px;
+            }
+            nav {
+                float: left;
+                max-width: 160px;
+                margin: 0;
+                padding: 1em;
+            }
+            article {
+                border-left: 1px solid gray;
+                padding: 1em;
+                overflow: hidden;
             }
 
             .type {
@@ -98,127 +148,146 @@
     <body ng-app="myApp">
 
         <div ng-controller="myCtrl">
-            <input type="text" ng-model="Keyword" placeholder="Search"/>
+            <header style="background-image: url('image/background/top.png'); background-size: 100% 100%;">
+                <h1 align="center">My Pokedex</h1>
+                <p>Here, you can search for info about pokemon (&copy Pokemon Company) that you need.</p>
+                <p>Current progress: Johto Pokemon</p>
+                <div align="center">
+                    <p>Search: <input type="text" style="color: black;" ng-model="Search.name" placeholder="Search" size="50px"/></p>
+                </div>
+                <br>
+            </header>
 
-            <accordion close-others="true">
-                <accordion-group ng-repeat="group in pokemon | filter:Keyword">
-                    <accordion-heading style="background-color: #000000">
-                        <img src="{{URL::asset("[[group.sprite]]")}}" alt="no_image" width="40px" height="40px">
-                        #[[ group.id ]] [[ group.name ]]
-                    </accordion-heading>
-                    <table border="0" style="width: 100%">
-                        <tr>
-                            <th style="text-align: center">
-                                <h1>[[ group.name ]]</h1>
-                                <table border="0" align="center">
-                                    <tr>
-                                        <td class="type [[type]]" width="45px" ng-repeat="type in group.type">
-                                            <span style="color:#FFF;"><b>[[type]]</b></span>
-                                        </td>
-                                    </tr>
-                                </table>
-                            </th>
-                        </tr>
-                        <tr>
-                            <td>
-                                <img src="{{URL::asset("[[group.image]]")}}" alt="no_image" width="300px" height="300px" align="left">
-                                <table style="border: 5px solid #000000;" align="right">
-                                    <tbody>
-                                    <tr>
-                                        <th id="heading" class="[[ group.type[0] ]]">Stat</th>
-                                    </tr>
+            <article style="background-image: url('image/background/middle.png'); background-size: 100% 100%;">
+                <accordion close-others="true">
+                    <accordion-group ng-repeat="group in filteredPokemon = (pokemon | filter:Search)">
+                        <accordion-heading style="background-color: #a00">
+                            <img src="{{URL::asset("[[group.sprite]]")}}" alt="no_image" width="40px" height="40px">
+                            #[[ group.id ]] [[ group.name ]]
+                        </accordion-heading>
+                        <table border="0" style="width: 100%">
+                            <tr>
+                                <th style="text-align: center" colspan="2">
+                                    <h1>[[ group.name ]]</h1>
+                                    <table border="0" align="center">
+                                        <tr>
+                                            <td class="type [[type]]" width="45px" ng-repeat="type in group.type">
+                                                <span style="color:#FFF;"><b>[[type]]</b></span>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </th>
+                            </tr>
+                            <tr>
+                                <td style="text-align: center;" colspan="1">
+                                    <img src="{{URL::asset("[[group.image]]")}}" alt="no_image" width="300px" height="300px">
+                                </td>
+                                <td>
+                                    <table style="border: 5px solid #000000;" align="center">
+                                        <tbody>
+                                        <tr>
+                                            <th id="heading" class="[[ group.type[0] ]]">Stat</th>
+                                        </tr>
 
-                                    <tr style="background: #FF5959;">
-                                        <td>
-                                            <table border="0" style="background: #FF0000; border: 1px solid #A60000;" cellspacing="0" cellpadding="0">
-                                                <tbody>
-                                                <tr>
-                                                    <th style="background: #FF5959; width: 75px;"><span style="color:#000;">HP:</span></th>
-                                                    <th style="background: #FF5959; width: 30px;"> [[group.base.hp]]</th>
-                                                    <td style="width: [[group.base.hp]]px;"></td>
-                                                </tr>
-                                                </tbody>
-                                            </table>
-                                        </td>
-                                    </tr>
+                                        <tr style="background: #FF5959;">
+                                            <td>
+                                                <table border="0" style="background: #FF0000; border: 1px solid #A60000;" cellspacing="0" cellpadding="0">
+                                                    <tbody>
+                                                    <tr>
+                                                        <th style="background: #FF5959; width: 75px;"><span style="color:#000;">HP:</span></th>
+                                                        <th style="background: #FF5959; width: 30px;"> [[group.base.hp]]</th>
+                                                        <td style="width: [[group.base.hp]]px;"></td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
 
-                                    <tr style="background: #F5AC78;">
-                                        <td>
-                                            <table style="background: #F08030; border: 1px solid #9C531F;" cellspacing="0" cellpadding="0">
-                                                <tbody>
-                                                <tr>
-                                                    <th style="background: #F5AC78; width: 75px;"><span style="color:#000;">Attack:</span></th>
-                                                    <th style="background: #F5AC78; width: 30px;"> [[group.base.attack]]</th>
-                                                    <td style="width: [[group.base.attack]]px;"></td>
-                                                </tr>
-                                                </tbody>
-                                            </table>
-                                        </td>
-                                    </tr>
-                                    <tr style="background: #FAE078;">
-                                        <td>
-                                            <table style="background: #F8D030; border: 1px solid #A1871F;" cellspacing="0" cellpadding="0">
-                                                <tbody>
-                                                <tr>
-                                                    <th style="background: #FAE078; width: 75px;"><span style="color:#000;">Defense:</span></th>
-                                                    <th style="background: #FAE078; width: 30px;"> [[group.base.defense]]</th>
-                                                    <td style="width: [[group.base.defense]]px;"></td>
-                                                </tr>
-                                                </tbody>
-                                            </table>
-                                        </td>
-                                    </tr>
-                                    <tr style="background: #9DB7F5;">
-                                        <td>
-                                            <table style="background: #6890F0; border: 1px solid #445E9C;" cellspacing="0" cellpadding="0">
-                                                <tbody>
-                                                <tr>
-                                                    <th style="background: #9DB7F5; width: 75px;"><span style="color:#000;">Sp.Atk:</span></th>
-                                                    <th style="background: #9DB7F5; width: 30px;"> [[group.base.spAtk]]</th>
-                                                    <td style="width: [[group.base.spAtk]]px;"></td>
-                                                </tr>
-                                                </tbody>
-                                            </table>
-                                        </td>
-                                    </tr>
-                                    <tr style="background: #A7DB8D;">
-                                        <td>
-                                            <table style="background: #78C850; border: 1px solid #4E8234;" cellspacing="0" cellpadding="0">
-                                                <tbody>
-                                                <tr>
-                                                    <th style="background: #A7DB8D; width: 75px;"><span style="color:#000;">Sp.Def:</span></th>
-                                                    <th style="background: #A7DB8D; width: 30px;"> [[group.base.spDef]]</th>
-                                                    <td style="width: [[group.base.spDef]]px;"></td>
-                                                </tr>
-                                                </tbody>
-                                            </table>
-                                        </td>
-                                    </tr>
-                                    <tr style="background: #FA92B2;">
-                                        <td>
-                                            <table style="background: #F85888; border: 1px solid #A13959;" cellspacing="0" cellpadding="0">
-                                                <tbody>
-                                                <tr>
-                                                    <th style="background: #FA92B2; width: 75px;"><span style="color:#000;">Speed:</span></th>
-                                                    <th style="background: #FA92B2; width: 30px;"> [[group.base.speed]]</th>
-                                                    <td style="width: [[group.base.speed]]px;"></td>
-                                                </tr>
-                                                </tbody>
-                                            </table>
-                                        </td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <h3>[[ group.description ]]</h3>
-                            </td>
-                        </tr>
-                    </table>
-                </accordion-group>
-            </accordion>
+                                        <tr style="background: #F5AC78;">
+                                            <td>
+                                                <table style="background: #F08030; border: 1px solid #9C531F;" cellspacing="0" cellpadding="0">
+                                                    <tbody>
+                                                    <tr>
+                                                        <th style="background: #F5AC78; width: 75px;"><span style="color:#000;">Attack:</span></th>
+                                                        <th style="background: #F5AC78; width: 30px;"> [[group.base.attack]]</th>
+                                                        <td style="width: [[group.base.attack]]px;"></td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                        <tr style="background: #FAE078;">
+                                            <td>
+                                                <table style="background: #F8D030; border: 1px solid #A1871F;" cellspacing="0" cellpadding="0">
+                                                    <tbody>
+                                                    <tr>
+                                                        <th style="background: #FAE078; width: 75px;"><span style="color:#000;">Defense:</span></th>
+                                                        <th style="background: #FAE078; width: 30px;"> [[group.base.defense]]</th>
+                                                        <td style="width: [[group.base.defense]]px;"></td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                        <tr style="background: #9DB7F5;">
+                                            <td>
+                                                <table style="background: #6890F0; border: 1px solid #445E9C;" cellspacing="0" cellpadding="0">
+                                                    <tbody>
+                                                    <tr>
+                                                        <th style="background: #9DB7F5; width: 75px;"><span style="color:#000;">Sp.Atk:</span></th>
+                                                        <th style="background: #9DB7F5; width: 30px;"> [[group.base.spAtk]]</th>
+                                                        <td style="width: [[group.base.spAtk]]px;"></td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                        <tr style="background: #A7DB8D;">
+                                            <td>
+                                                <table style="background: #78C850; border: 1px solid #4E8234;" cellspacing="0" cellpadding="0">
+                                                    <tbody>
+                                                    <tr>
+                                                        <th style="background: #A7DB8D; width: 75px;"><span style="color:#000;">Sp.Def:</span></th>
+                                                        <th style="background: #A7DB8D; width: 30px;"> [[group.base.spDef]]</th>
+                                                        <td style="width: [[group.base.spDef]]px;"></td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                        <tr style="background: #FA92B2;">
+                                            <td>
+                                                <table style="background: #F85888; border: 1px solid #A13959;" cellspacing="0" cellpadding="0">
+                                                    <tbody>
+                                                    <tr>
+                                                        <th style="background: #FA92B2; width: 75px;"><span style="color:#000;">Speed:</span></th>
+                                                        <th style="background: #FA92B2; width: 30px;"> [[group.base.speed]]</th>
+                                                        <td style="width: [[group.base.speed]]px;"></td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <h3>[[ group.description ]]</h3>
+                                </td>
+                            </tr>
+                        </table>
+                    </accordion-group>
+                    <div ng-hide="filteredPokemon.length" style="text-align: center">
+                        No Result
+                    </div>
+                </accordion>
+            </article>
+
+            <footer style="background-image: url('image/background/bottom.png'); background-size: 100% 100%;">
+                <p>Made by: Kevin Jonathan Koswara<br>Institut Teknologi Bandung<br>Teknik Informatika</p>
+            </footer>
         </div>
 
     </body>
